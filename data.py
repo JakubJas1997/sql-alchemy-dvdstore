2d97bcf4b86a044447141d04ee6122744500d525
Uzytkownik <uzytkownik@kursGIT.pl>
2023-06-04 20:00:10 +0200

    sample data

---
+++ data.py
@@ -0,0 +1,11 @@
+film_categories = ['action', 'comedy', 'drama', 'horror', 'romance',
+                   'thriller', 'biography', 'musical', 'fantasy', 'science-fiction']
+
+film_languages = ['english', 'french', 'german', 'italian', 'japanese',
+                  'korean', 'portuguese', 'hindu', 'polish', 'spanish']
+
+countries = ['USA', 'Canada', 'France', 'Germany', 'Italy', 'Japan', 'Spain', 'UK', 'Poland', 'Korea']
+
+us_cities = ['New York', 'Los Angeles', 'Chicago', 'New York', 'Washington', 'San Francisco']
+
+can_cities = ['Vancouver', 'Montreal', 'Ottawa','Toronto', 'Calgary']
